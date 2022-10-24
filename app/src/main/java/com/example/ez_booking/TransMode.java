1dcaa09317f79ef0e9c84b627e75231dfd245a04
Nirmal Diaz <nirmaldiaz@gmail.com>
2022-10-24 09:32:57 +0530

    Added: Event listener for next button

--- app/src/main/java/com/example/ez_booking/TransMode.java
+++ app/src/main/java/com/example/ez_booking/TransMode.java
@@ -2,7 +2,9 @@ package com.example.ez_booking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TransMode extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class TransMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans_mode);
+
+        Button btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(view -> {
+            Intent intent = new Intent(TransMode.this, destination.class);
+            startActivity(intent);
+        });
     }
 }
