4be4a52dbbd4827d643f5a8bc1fc1792e97c983b
DESKTOP-NO49A75\Acer <dpsasi10@gmail.com>
2022-10-24 08:42:52 +0530

    third commit

--- app/src/main/java/com/example/ez_booking/destination.java
+++ app/src/main/java/com/example/ez_booking/destination.java
@@ -2,33 +2,83 @@ package com.example.ez_booking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class destination extends AppCompatActivity {
+import java.text.DateFormatSymbols;
 
-    private TextView tvSourceSpinner, tvDestinationSpinner;
-    private Spinner sourceSpinner, destinationSpinner;
-    private ArrayAdapter<CharSequence> sourceAdapter, destinationAdapter;
+public class destination extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinnerSource, spinnerDestination;
+    Button btnNext2;
+    //String[] source;
+    private Build.VERSION_CODES android;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
 
-        //Source spinner initialisation
-        sourceSpinner = findViewById(R.id.spinnerSource);
+        spinnerSource = findViewById(R.id.spinnerSource);
+        spinnerDestination = findViewById(R.id.spinnerDestination);
+        btnNext2 = findViewById(R.id.btnNext2);
 
-        //ArrayAdapter for Source
-        sourceAdapter = ArrayAdapter.createFromResource(this, R.array.array_source,R.layout.spinner_layout);
+        populateSpinnerSource();
+        populateSpinnerDestination();
 
-        //Specify the layout to use when the list of choices appear
+        btnNext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String source = spinnerSource.getSelectedItem().toString();
+                String destination = spinnerDestination.getSelectedItem().toString();
+                //Toast.makeText(activity_destination.this, source + ", "+ destination,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        spinnerSource.setOnItemSelectedListener(this);
+        spinnerDestination.setOnItemSelectedListener(this);
+
+
+    }
+
+    private void populateSpinnerSource() {
+        //source = new DateFormatSymbols().getSource();
+        ArrayAdapter<String>sourceAdapter = new ArrayAdapter<>(this.android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.array_source));
         sourceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSource.setAdapter(sourceAdapter);
+    }
 
-        // Set the adapter to the spinner to populate the source spinner
-        sourceSpinner.setAdapter(sourceAdapter);
+    private void populateSpinnerDestination() {
+        ArrayAdapter<String> destinationAdapter = new ArrayAdapter<>(this.android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.array_destination));
+        destinationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDestination.setAdapter(destinationAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        if(parent.getId() == R.id.spinnerSource){
+            //String selectedSource = parent.getSelectedItem.toString();
+            String selectedSource = parent.getItemPosition(position).toString();
+            Toast.makeText(this,"Selected: "+selectedSource,Toast.LENGTH_SHORT).show();
+        }else if(parent.getId() == R.id.spinnerDestination){
+            String selectedDestination = parent.getSelectedItem.toString();
+            Toast.makeText(this,"Selected: "+selectedDestination,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
 
     }
 }
