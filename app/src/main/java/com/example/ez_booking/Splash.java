7de50f68e6c6d6cfc133255a8e443fe11b68d090
= <=>
2022-10-30 17:26:38 +0530

    Fixed: User not checked on splash

--- app/src/main/java/com/example/ez_booking/Splash.java
+++ app/src/main/java/com/example/ez_booking/Splash.java
@@ -8,19 +8,33 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Splash extends AppCompatActivity {
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mAuth = FirebaseAuth.getInstance();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this,TransMode.class);
-                finish();
-                startActivity(intent);
+                if (mAuth.getCurrentUser() == null) {
+                    Intent intent = new Intent(Splash.this,LoginActivity.class);
+                    finish();
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(Splash.this,TransMode.class);
+                    finish();
+                    startActivity(intent);
+                }
+
+
             }
         },3000);
     }
